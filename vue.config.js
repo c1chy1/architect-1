a45adb51612725618412db2b17df767e31f31350
c1chy1 <cichy08081987pp@gmail.com>
2022-02-21 16:40:10 +0100

    vue config

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+
+
+    pwa: {
+        name: 'Acme Studios',
+        themeColor: '#F0ECE3',
+        msTileColor: '#596E79',
+
+    },
+
+}
+
+
